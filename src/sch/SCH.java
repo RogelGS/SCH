e17cda62b40136cad890581f3a977bcef61c4005
RogelGS <rogel.gnz.snz@gmail.com>
2020-12-12 23:08:27 -0600

    Creacion de la Ventana de Logueo

--- src/sch/SCH.java
+++ src/sch/SCH.java
@@ -5,6 +5,8 @@
  */
 package sch;
 
+import view.Login;
+
 /**
  *
  * @author KSGamer
@@ -15,7 +17,8 @@ public class SCH {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
